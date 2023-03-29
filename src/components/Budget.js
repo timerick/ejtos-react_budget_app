78cd79cdda3507197a818971036532a80fc1a6ca
tmr <tmryckman@gmail.com>
2023-03-29 15:53:46 -0400

    added budget change ability

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,27 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget, currency, } = useContext(AppContext);
+    const { dispatch, budget, currency, } = useContext(AppContext);
+    const changeBudget = (val)=>{
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: val,
+        })
+}
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: {currency}{budget}</span>
+            <span>Budget: {currency}</span>
+            <input
+                required='required'
+                type='number'
+                id='budget'
+                value={budget}
+                step={10}
+                style={{ width: 80 }}
+                onChange={(event) => changeBudget(event.target.value)
+                }
+            />
         </div>
     );
 };
